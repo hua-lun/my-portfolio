a8d1eb45cde74131964803863fb3518db801767f
Hua Lun Chua <chuahualun@gmail.com>
2020-06-07 22:45:55 +0800

    Add list of messages

--- portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/DataServlet.java
@@ -15,6 +15,8 @@
 package com.google.sps.servlets;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -24,9 +26,24 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/data")
 public class DataServlet extends HttpServlet {
 
+  private List<String> messages;
+
+  @Override
+  public void init() {
+      messages = new ArrayList<>();
+      messages.add("no pomegranates");
+      messages.add("you could stop at five or six stores or just one");
+      messages.add("stop it, get some help");
+      messages.add("beep beep I'm a sheep");
+      messages.add("why are you running!?");
+      messages.add("wait... what?");
+  }
+
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
+    String message = messages.get((int) (Math.random() * messages.size()));
+
     response.setContentType("text/html;");
-    response.getWriter().println("<h1>Hello Hua Lun!</h1>");
+    response.getWriter().println(message);
   }
 }
