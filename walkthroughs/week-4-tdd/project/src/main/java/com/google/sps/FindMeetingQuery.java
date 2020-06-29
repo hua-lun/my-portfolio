e69440c55a8f2a618a034b2d6f84ba8d012efe7d
Hua Lun Chua <chuahualun@gmail.com>
2020-06-29 22:51:16 +0800

    Change var names and use comsumer

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -19,9 +19,10 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.function.Consumer;
 import java.util.List;
 
-public final class FindQuery {
+public final class FindMeetingQuery {
     public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
 
         // Get attendees and duration
@@ -29,76 +30,74 @@ public final class FindQuery {
         long duration = request.getDuration();
 
         // Create list to store available timings
-        Collection<TimeRange> times = new ArrayList<>();
+        Collection<TimeRange> availableTimings = new ArrayList<>();
 
         // if there are no attendees, return the whole day
         if (attendees.isEmpty()) {
-            times.add(TimeRange.WHOLE_DAY);
-            return times;
+            availableTimings.add(TimeRange.WHOLE_DAY);
+            return availableTimings;
         }
 
         // if the duration is longer than a day, return no timings (empty list)
         if (duration >= TimeRange.WHOLE_DAY.duration() + 1) {
-            return times;
+            return availableTimings;
         }
 
         // get unavailable event timings
         List<Event> unavailableTimings = getUnavailableTimings(events, attendees);
 
         // get available event timings
-        times = getAvailableTimings(unavailableTimings, duration);
+        availableTimings = getAvailableTimings(unavailableTimings, duration);
 
-        return times;
+        return availableTimings;
     }
 
     // Get unavailable timings for events, sort events and return the list of timings
     private List<Event> getUnavailableTimings(Collection<Event> events, Collection<String> attendees) {
         List<Event> unavailableTimings = new ArrayList<>();
-        for (Event e : events) {
+        Consumer<Event> consumer = s -> {
             for (String a : attendees) {
-                if (e.getAttendees().contains(a)) {
-                    unavailableTimings.add(e);
+                if (s.getAttendees().contains(a)) {
+                    unavailableTimings.add(s);
                     break;
                 }
             }
-        }
-
-        Comparator<Event> compareStartingTime =
-                (Event e1, Event e2) -> TimeRange.ORDER_BY_START.compare(e1.getWhen(), e2.getWhen());
-        Collections.sort(unavailableTimings, compareStartingTime);
-
+        };
+        events.forEach(consumer);
+        
+        Collections.sort(unavailableTimings, TimeRange.ORDER_BY_START);
         return unavailableTimings;
     }
 
     // Get TimeRange of available timings
     private Collection<TimeRange> getAvailableTimings(List<Event> unavailableTimings, long duration) {
-        Collection<TimeRange> times = new ArrayList<>();
+        Collection<TimeRange> timeRangeList = new ArrayList<>();
 
         int startTime = TimeRange.START_OF_DAY;
         int closingTime = TimeRange.END_OF_DAY;
 
         for (Event e : unavailableTimings) {
-            TimeRange tr = e.getWhen();
+            TimeRange timeRange = e.getWhen();
 
             // check if event can be added before the start time
-            if (startTime + duration <= tr.start()) {
-                TimeRange trBefore = TimeRange.fromStartEnd(startTime, tr.start(), false);
-                times.add(trBefore);
+            if (startTime + duration <= timeRange.start()) {
+                TimeRange timeAvailableBeforeEvent = TimeRange.fromStartEnd(startTime, timeRange.start(), false);
+                timeRangeList.add(timeAvailableBeforeEvent);
                 // since event is added, the start time == end time
-                startTime = tr.end();
+                startTime = timeRange.end();
 
                 // if there is not enough time, change start time == end time
-            } else if (startTime < tr.end()) {
-                startTime = tr.end();
+            } else if (startTime < timeRange.end()) {
+                startTime = timeRange.end();
             }
 
         }
         // check if an event can be added in before closing time
         if (closingTime - startTime >= duration) {
-            TimeRange trAfter = TimeRange.fromStartEnd(startTime, TimeRange.END_OF_DAY, true);
-            times.add(trAfter);
+            TimeRange timeAvailableBeforeClosing = TimeRange.fromStartEnd(startTime, TimeRange.END_OF_DAY, true);
+            timeRangeList.add(timeAvailableBeforeClosing);
         }
-        return times;
+        return timeRangeList;
     }
 
 }
